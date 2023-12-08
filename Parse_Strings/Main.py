01c9823bc3b9d7a5d7b887681e63cabe5f515a95
Raul S Medina <scoobym05@outlook.com>
2023-12-08 14:11:34 -0700

    resumbiting

--- Parse_Strings/Main.py
+++ Parse_Strings/Main.py
@@ -1,26 +1,21 @@
 import re
 
 def parser(instring):
-    try:
-        literal_Python=eval(instring)
 
-        if isinstance(instring, str):
-            return True
-        else:
-            return False
-    except:
-        return False
+    pattern=re.compile(r'^[\'\"](?:[^\\\\\'\"]|\\\\[nrt\\\"])*[\'\"]$') #pattern to match a python string literal
+
+    return bool(pattern.match(instring))#checks to see if phrase user inputed is a python string literal
 
 def main():
     try:
-        uInput=input("Enter a phrase to check if it is a python string litiral: ")
+        uInput=input("Enter a phrase to check if it is a python string litiral: ")#user inputs a phrase of there chooseing
     
         if parser(uInput):
             print("Good job the phrase you entered is a string literal.")
         else:
             print("The phrase you entered is not a string literial please try agian.")
     except EOFError:
-        print ("An error has occured please provide a input.")
+        print ("An error has occured please provide a input.")#if you did not enter a phrase to compare with it will error out with an EOFError and tell you to put in a pharse.
 
 
 if __name__ == "__main__":
